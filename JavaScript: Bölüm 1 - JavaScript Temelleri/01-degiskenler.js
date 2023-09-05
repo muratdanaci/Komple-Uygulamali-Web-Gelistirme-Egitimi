e85a2feee614ea118c37ab7ea5c50602bd11ecb1
Murat Danaci <muratdanaci8@gmail.com>
2023-09-05 13:15:28 +0300

    Full Stack

---
+++ JavaScript: Bölüm 1 - JavaScript Temelleri/01-degiskenler.js
@@ -0,0 +1,7 @@
+var maasAli = 5000;
+var maasCan = 6000;
+var zam = 0.3;
+
+
+console.log(maasAli + (maasAli * zam)); // Maas Ali
+console.log(maasCan + (maasCan * zam)); // Maas Can
---
+++ JavaScript: Bölüm 1 - JavaScript Temelleri/02-degisken_turleri.js
@@ -0,0 +1,36 @@
+var urunAdi = "iphone 14";
+let urunFiyat = 40000;
+
+console.log(typeof urunAdi)
+console.log(typeof urunFiyat)
+
+
+let sayi1 = "10";
+let sayi2 = "20";
+
+console.log(sayi1 + sayi2);
+console.log(Number(sayi1) + Number(sayi2));
+
+
+let sayi3 = 30;
+let sayi4 = 40;
+
+console.log(sayi3.toString() + sayi4.toString());
+
+
+let isim = "Murat";
+let soyisim = "Danaci";
+
+console.log(isim + " " + soyisim);
+
+
+let sinavNotu = 70;
+let basarilimi = (sinavNotu >= 50);
+
+console.log(basarilimi);
+
+
+let yas;
+
+console.log(yas);
+console.log(typeof yas);
